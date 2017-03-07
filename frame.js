51a840fe997390dfa4811c5533d57167d173403b
Joshua R. Engel <joshua@68studios.io>
2017-03-07 17:29:11 -0600

    Pass 2 more tests but smelly & dirty code to do so

--- frame.js
+++ frame.js
@@ -1,23 +1,26 @@
 function Frame(isFinalFrame){
+  
   //Constructor
-  this.pinCount = 10;
+  var missChancePerPin = 6.4;
+  var baseMissChance = 18;
+  var maxPinCount = 10;
+
+  this.pinCount = maxPinCount;
+  this.missChance = baseMissChance;
   this.attemptLimit = 2;
   if(isFinalFrame) { this.attemptLimit = 3; };
-  this.missChance = 18;
-
-  //Each pin variation modifies the miss chance for the frame
-  var missChancePerPin = 6.4;
 
   //Implementation
-  UpdateMissChance = function(){
-    this.missChance = (missChancePerPin * this.pinCount);
-  }
+  /*var UpdateMissChance = function(){
+      return baseMissChance + (missChancePerPin * (maxPinCount - this.pinCount));
+  };
+*/
   //Interface
   this.adjustPins = function(number){
     this.pinCount += number;
     if (this.pinCount > 10) { this.pinCount = 10; };
     if (this.pinCount < 0) { this.pinCount = 0; };
-    console.log(this.missChance);
+    this.missChance = 82 - (this.pinCount * missChancePerPin);
   }
 
   this.resetPins = function(){
